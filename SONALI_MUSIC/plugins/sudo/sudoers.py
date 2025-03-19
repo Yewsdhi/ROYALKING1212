b6a89a6975307101d45071f984b3d0a311b653aa
Badnam xd <yadavxd746@gmail.com>
2025-03-19 22:37:32 +0530

    Update sudoers.py

--- SONALI_MUSIC/plugins/sudo/sudoers.py
+++ SONALI_MUSIC/plugins/sudo/sudoers.py
@@ -61,7 +61,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝗥ᴀᴜsʜᴀɴ ᴋᴀ 𝗟ᴜɴᴅ 𝗟ᴇɢᴀ 😂 \n 𝗦ᴜᴅᴏʟɪsᴛ 𝗗ᴇᴋʜɴᴇ ᴀᴀʏᴀ ᴍᴄ 🤧🖕", show_alert=True)
+        return await callback_query.answer("༎ࠫ🫧⛧‌ٖٖٖٖٖٖٜٖٖٖٖ 𝑹⌾𝒀𝜜𝑳𝆺𝅥˶꯭꯭꯭֟፝͟͢͝⏎͟›͢〖 ᴋᴀ 𝗟ᴜɴᴅ 𝗟ᴇɢᴀ 😂 \n 𝗦ᴜᴅᴏʟɪsᴛ 𝗗ᴇᴋʜɴᴇ ᴀᴀʏᴀ ᴍᴄ 🤧🖕", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
