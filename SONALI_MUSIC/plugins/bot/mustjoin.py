d5c67e3b17179f9e72f939e725d32dc1247cbf91
Badnam xd <yadavxd746@gmail.com>
2025-03-19 22:43:59 +0530

    Update mustjoin.py

--- SONALI_MUSIC/plugins/bot/mustjoin.py
+++ SONALI_MUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SONALI_MUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "PURVI_SUPPORT"
+MUST_JOIN = "TEAM_BADNAM_BOTS"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -30,7 +30,7 @@ async def must_join_channel(app: Client, msg: Message):
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
                             ],
                             [
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ONE_WAS_SIGMA"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/TEAM_BADNAM_BOTS"),
                             ]
                         ]
                     )
